e4a686ee4e9fe20d86d778bc63ea2189d4d3e4f2
Stefan Salanski <stefan.salanski@sapns2.com>
2021-12-02 00:00:46 -0500

    blank file for day 2

---
+++ src/Day02.kt
@@ -0,0 +1,18 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 7)
+    //check(part2(testInput) == 5)
+
+    val input = readInput("Day02")
+    println("part 1: ${part1(input)}")
+    println("part 2: ${part2(input)}")
+}
