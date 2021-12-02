983f71bc5d836c1344caf88f5e181f3d21620b18
Stefan Salanski <stefan.salanski@sapns2.com>
2021-12-02 00:14:53 -0500

    add similar solution for day 2 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -18,13 +18,29 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        var horizontalPos = 0
+        var depth = 0
+        var aim = 0
+        input.forEach { line ->
+            val (direction,amount) = line.split(" ")
+            if (direction == "up") {
+                aim -= amount.toInt()
+            }
+            else if (direction == "down") {
+                aim += amount.toInt()
+            }
+            else if (direction == "forward") {
+                horizontalPos += amount.toInt()
+                depth += aim * amount.toInt()
+            }
+        }
+        return depth * horizontalPos
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 150)
-    //check(part2(testInput) == 5)
+    check(part2(testInput) == 900)
 
     val input = readInput("Day02")
     println("part 1: ${part1(input)}")
