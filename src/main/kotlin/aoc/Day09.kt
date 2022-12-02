111f70c40cadf93c4d75c74106ebfa8a70e34aaa
Stefan Salanski <stefan.salanski@sapns2.com>
2022-12-02 18:20:57 -0500

    day 9 part 1

---
+++ src/main/kotlin/aoc/Day09.kt
@@ -0,0 +1,53 @@
+package aoc
+
+class Day09 : Day(9) {
+  override fun part1(input: List<String>): Int {
+    val heightmap = input.map{ it.map { it.digitToInt() }}
+
+    val localMinInMap = emptyList<Pair<Int,Int>>().toMutableList()
+    heightmap.forEachIndexed { rowidx, row ->
+      val localMinInRow = getLocalMinIdx(row)
+      localMinInRow.forEach { localMinColIdx ->
+        val isFirstRow = rowidx == 0
+        val isLastRow = rowidx == heightmap.size-1
+        fun prevRowHigher() = (heightmap[rowidx-1][localMinColIdx] > row[localMinColIdx])
+        fun nextRowHigher() = (heightmap[rowidx+1][localMinColIdx] > row[localMinColIdx])
+        if( (isFirstRow || prevRowHigher()) && (isLastRow || nextRowHigher())  ) {
+          localMinInMap.add(Pair(rowidx,localMinColIdx))
+        }
+      }
+    }
+//    println("found ${localMinInMap.size} local minima")
+//    localMinInMap.forEach { println(it) }
+
+    return localMinInMap.map { (r,c) -> heightmap[r][c] }.sumOf { it + 1 }
+  }
+
+  override fun part2(input: List<String>): Int {
+    return 0
+  }
+
+  override fun check1(input: List<String>): Boolean {
+    return (part1(input) == 15)
+  }
+
+  override fun check2(input: List<String>): Boolean {
+    return (part2(input) == -1)
+  }
+}
+
+fun getLocalMinIdx(lst: List<Int>): List<Int> {
+  val localMins = mutableListOf<Int>()
+  if( lst[0] < lst[1] ) {
+    localMins.add(0)
+  }
+  for(i in 1 until lst.size-1) {
+    if (lst[i-1] > lst[i] && lst[i] < lst[i+1] ) {
+      localMins.add(i)
+    }
+  }
+  if( lst[lst.size-2] > lst[lst.size-1] ) {
+    localMins.add(lst.size-1)
+  }
+  return localMins
+}
